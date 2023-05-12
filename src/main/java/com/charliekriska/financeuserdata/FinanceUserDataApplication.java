9255eac90c39890a760ccf7bca87f57755feddf5
cjkriska <charleskriska@gmail.com>
2023-05-12 15:13:58 -0500

    initial commit

---
+++ src/main/java/com/charliekriska/financeuserdata/FinanceUserDataApplication.java
@@ -0,0 +1,13 @@
+package com.charliekriska.financeuserdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceUserDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceUserDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/charliekriska/financeuserdata/FinanceUserDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charliekriska.financeuserdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceUserDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
