aea5b780ba053ed37f1485bf57dfdfee52125d23
cjkriska <charleskriska@gmail.com>
2023-05-12 15:32:41 -0500

    add sample endpoint and its unit test

---
+++ src/main/java/com/charliekriska/financeuserdata/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.charliekriska.financeuserdata.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/user")
+    public String getUser() {
+        return "Sample User";
+    }
+
+}
---
+++ src/test/java/com/charliekriska/financeuserdata/controller/UserControllerTest.java
@@ -0,0 +1,30 @@
+package com.charliekriska.financeuserdata.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.core.StringContains.containsString;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void shouldReturnSampleUser() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/user"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Sample User")));
+    }
+
+}
