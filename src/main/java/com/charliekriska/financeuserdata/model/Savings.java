506948f3b1cade0eca6dce38c6eece0d62d3fac3
cjkriska <charleskriska@gmail.com>
2023-08-30 16:34:00 -0500

    add controller testing for both get savings by user id and post savings

--- src/main/java/com/charliekriska/financeuserdata/model/Savings.java
+++ src/main/java/com/charliekriska/financeuserdata/model/Savings.java
@@ -1,9 +1,21 @@
 package com.charliekriska.financeuserdata.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 public class Savings {
+
+    public Savings() {
+    }
+
+    public Savings(int userId, double income, double expenses, double currentSavings, double expectedReturn) {
+        this.userId = userId;
+        this.income = income;
+        this.expenses = expenses;
+        this.currentSavings = currentSavings;
+        this.expectedReturn = expectedReturn;
+    }
 
     private int savingsDataId;
     private int userId;
